2d1ff56f0fd3dc78b8d6e3bf93e8fc2a150be3e2
Felipe <felipeandreshenriquez2004@gmail.com>
2026-06-17 20:01:13 -0400

    swagger agregado

--- src/main/java/com/hotel/ms_checkin/config/SecurityConfig.java
+++ src/main/java/com/hotel/ms_checkin/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        // --- PASE PARA SWAGGER CORREGIDO AQUÍ ---
+                        // --- PASE PARA SWAGGER AQUÍ ---
                         .requestMatchers("/v3/api-docs", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
